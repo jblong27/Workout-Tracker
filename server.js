a17bc4483f949c3af95e7c7502463005299a705d
Jordan Long <jblong27@gmail.com>
2020-09-08 22:27:20 -0500

    complete server.js, added routes

---
+++ server.js
@@ -0,0 +1,23 @@
+const express =  require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//use mongoose to connect to our database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/swoletracker", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"))
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
