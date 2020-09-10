2bc939e2f8f5e3360e9a632a00700119b491809d
Jordan Long <jblong27@gmail.com>
2020-09-10 13:42:13 -0500

    get route added to api.js

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout = require("../models/workout.js");
+const Workout = require("../models");
 
 router.post("/api/workouts", ({ body }, res) => {
   Workout.create(body)
@@ -13,6 +13,11 @@ router.post("/api/workouts", ({ body }, res) => {
 
 router.get("/api/workouts", (req, res) => {
   Workout.find({})
-    //
-})
+    .then(dbWorkout => { 
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
 module.exports = router;
