667ba1496f9f6fd6ab934da1316a0363c2cb4e64
Jordan Long <jblong27@gmail.com>
2020-09-10 12:51:00 -0500

    added get routes for exercise and stats html

--- routes/html.js
+++ routes/html.js
@@ -2,4 +2,10 @@ const path = require("path");
 
 module.exports = app => {
   // get routes here
-}
+  app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+  app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
+  });
+};
